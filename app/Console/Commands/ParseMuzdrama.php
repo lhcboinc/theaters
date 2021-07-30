696021b3c53f7e407b0ff78973d915ca55258333
Lhc Boinc <lhc_boinc@yahoo.com>
2021-07-30 11:39:37 +0300

    changes

--- app/Console/Commands/ParseMuzdrama.php
+++ app/Console/Commands/ParseMuzdrama.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\Http;
 
 class ParseMuzdrama extends Command
 {
+    protected static $host = 'https://muzdrama.ru';
+
     /**
      * The name and signature of the console command.
      *
@@ -39,7 +41,7 @@ class ParseMuzdrama extends Command
      */
     public function handle()
     {
-        self::parseMonth("https://muzdrama.ru/?month=7");
+        self::parseMonth(self::$host . "/?month=7");
         return 0;
     }
 
@@ -51,7 +53,47 @@ class ParseMuzdrama extends Command
         libxml_use_internal_errors(true);
         $doc->loadHTML($response);
         $finder = new \DomXPath($doc);
-        $node = $finder->query("//*[contains(concat(' ', normalize-space(@id), ' '), 'afisha')]/div");
-        print_r($node);
+        $nodes = $finder->query("//*[contains(concat(' ', normalize-space(@id), ' '), 'afisha')]/div");
+        foreach ($nodes as $node) {
+            $item = $finder->query(".//div/a", $node);
+            $href = $item->item(0)->attributes->item(0)->value;
+            $item = $finder->query(".//div/div[@class='booking__price']/b", $node);
+            $price = $item->item(0)->nodeValue;
+            self::parsePerformance(self::$host . $href);
+        }
+    }
+
+    private static function parsePerformance($url)
+    {
+        $month = ['сентября' => 'september'];
+        $imgSrc = null;
+        $response = Http::get($url);
+        if (!$response) throw new \Exception('Error reading page, stopped');
+        $doc = new \DOMDocument();
+        libxml_use_internal_errors(true);
+        $doc->loadHTML($response);
+        $finder = new \DomXPath($doc);
+        $root = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), 'afisha__item afisha__item_content flex flex-wrap')]");
+        $item = $finder->query(".//div[@class='afisha__image-wrapper afisha__image-wrapper_content']/a/img", $root->item(0));
+        foreach ($item->item(0)->attributes as $attribute) {
+            if ($attribute->name == 'src') {
+                $imgSrc = $attribute->value;
+            }
+        }
+        print $imgSrc."\n";
+        $liElements = $finder->query(".//div[@class='afisha__info afisha__info_content performance flex flex-column']/ul[@class='performance__dates']/li", $root->item(0));
+        $dateTimes = null;
+        if ($liElements->length > 0) {
+            $dateTimes = [];
+            foreach ($liElements as $element) {
+                list($date, $startTime) = explode(' в ', trim($element->nodeValue, ';'));
+                $dateTimes[] = date("Y-m-d H:i", strtotime(str_replace(array_keys($month), $month, $date)." ".$startTime));
+            }
+        }
+        $element = $finder->query(".//div[@class='afisha__info afisha__info_content performance flex flex-column']/div[@class='performance__duration']/span", $root->item(0));
+        $duration = trim(@$element->item(0)->nodeValue, ' ч.');
+        $element = $finder->query(".//div[@class='afisha__info afisha__info_content performance flex flex-column']/h1[@class='performance__title']", $root->item(0));
+        $title = @$element->item(0)->nodeValue;
+        $element = $finder->query(".//div[@class='afisha__info afisha__info_content performance flex flex-column']/h1[@class='performance__title']", $root->item(0));
     }
 }
