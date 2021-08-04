8e494c43cfaa7612867eb051704ade622cf7adde
Lhc Boinc <lhc_boinc@yahoo.com>
2021-08-04 17:01:55 +0300

    add muzdrama seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Theaters;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +15,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        Theaters::insert([
+            'name' => 'ДОНЕЦКИЙ ГОСУДАРСТВЕННЫЙ АКАДЕМИЧЕСКИЙ МУЗЫКАЛЬНО-ДРАМАТИЧЕСКИЙ ТЕАТР ИМЕНИ М.М.БРОВУНА',
+            'domain_name' => 'muzdrama.ru',
+        ]);
     }
 }
